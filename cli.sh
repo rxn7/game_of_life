9825fda23da56652b6d72c04329396355bec12ff
rxn <rotthin_dev@protonmail.com>
2022-08-29 16:53:55 +0200

    Removed dashes (-) from arguments in cli.sh

--- cli.sh
+++ cli.sh
@@ -1,33 +1,33 @@
 #!/usr/bin/env bash
 
-print_arg_desc() {
+arg_desc() {
 	echo -e "\e[1;34m$1\e[0;0m: $2"
 }
 
 show_help() {
 	echo -e "\e[1;35mrxn's Game of Life CLI\n"
-	print_arg_desc "-help" "Show possible arguments"
-	print_arg_desc "-linux" "Build the game for linux"
-	print_arg_desc "-run" "Build the game for linux and run it natively"
-	print_arg_desc "-clean" "Clean the obj and bin directories"
+	arg_desc "help" "Show possible arguments"
+	arg_desc "linux" "Build the game for linux"
+	arg_desc "run" "Build the game for linux and run it natively"
+	arg_desc "clean" "Clean the obj and bin directories"
 }
 
 for arg in "$@"; do
-	if [ "$arg" = "-help" ]; then
+	if [ "$arg" = "help" ]; then
 		show_help
 		exit
 
-	elif [ "$arg" = "-linux" ] || [ "$arg" = "-lin" ]; then
+	elif [ "$arg" = "linux" ]; then
 		make -f linux.mk
 		exit
 
-	elif [ "$arg" = "-run" ]; then
+	elif [ "$arg" = "run" ]; then
 		echo -e "\e[1;32mBuilding the game for linux and running it natively...\e[0;0m"
 		make -f linux.mk
 		pushd bin/linux ; ./game_of_life ; popd
 		exit
 
-	elif [ "$arg" = "-clean" ]; then
+	elif [ "$arg" = "clean" ]; then
 		rm -rf obj
 		rm -rf bin
 		exit
